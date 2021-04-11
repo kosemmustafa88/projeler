ff7af01c85a371b8a495c60158be3790201079f9
kosemmustafa88 <kosemmustafa88@gmail.com>
2021-04-11 02:01:00 +0200

    Initial commit

---
+++ HTML_Kodlama/Kendi Projem ve Odev Uygulamasi/mail/gonder2.php
@@ -0,0 +1,30 @@
+﻿
+<meta http-equiv="refresh" content="2;URL= http://test.digital-mann.de/iletisim.html">
+
+<?
+require("class.phpmailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                   // send via SMTP
+$mail->Host     = "smtp.hostinger.web.tr"; // SMTP servers
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "admin@digital-mann.de";  // SMTP username
+$mail->Password = "M.k12121988"; // SMTP password
+$mail->CharSet = 'UTF-8';
+$mail->Port = 587;
+$mail->From     = "admin@digital-mann.de"; // smtp kullanycy adynyz ile ayny olmaly
+$mail->Fromname = "giden ismi";
+$mail->AddAddress("admin@digital-mann.de","Websitenizden gelen Sipariþiniz.");
+$mail->Subject  =  $_POST['isim'];
+$mail->Body     =  implode("  ",$_POST);
+if(!$mail->Send())
+{
+   echo "Mesaj Gönderilemedi <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo '<script type="text/javascript">alert("E Postanız  Başarı ile  Gönderilmiştir.");</script>';
+
+?>
